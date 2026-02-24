de0713e44fe8a5ac00854bb90b5d6d4b0e0f686e
VSCRM <161849246+VSCRM@users.noreply.github.com>
2026-02-24 13:17:08 +0200

    Refactor BookMenu class structure and comments

--- CW1/src/main/java/library/menu/BookMenu.java
+++ CW1/src/main/java/library/menu/BookMenu.java
@@ -7,53 +7,15 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-/**
- * Клас {@code BookMenu} представляє підменю для роботи з книгами в бібліотечній системі.
- * <p>
- * Цей клас забезпечує інтерфейс для користувача, що дозволяє:
- * <ul>
- *   <li>Переглядати всі книги</li>
- *   <li>Переглядати книги в порядку популярності</li>
- *   <li>Повернутися до головного меню</li>
- * </ul>
- * Клас використовує об'єкт {@code DBOutput}, для виведення інформації про книги відповідно.
- * </p>
- * Методи:
- * <ul>
- *   <li>{@link #BookMenu()} - Конструктор класу, який ініціалізує об'єкт {@code DBOutput}.</li>
- *   <li>{@link #display()} - Відображає підменю "Книги" і обробляє вибір користувача.
- *       <ul>
- *         <li>{@code @throws SQLException} Якщо виникає помилка при доступі до бази даних.</li>
- *         <li>{@code @throws IOException} Якщо виникає помилка при роботі з файлами.</li>
- *       </ul>
- *   </li>
- * </ul>
- */
 public class BookMenu {
     private final DBOutput output;
     private final Search search = new Search();
 
-    /**
-     * Конструктор {@code BookMenu} ініціалізує об'єкт {@code DBOutput},
-     * який використовуються для виведення інформації про книги.
-     */
+    
     public BookMenu() {
         output = new DBOutput();
     }
 
-    /**
-     * Метод {@code display()} відображає підменю для роботи з книгами й обробляє вибір користувача.
-     * <p>
-     * Варіанти меню:
-     * <ul>
-     *   <li>1 - Вивести всі книги</li>
-     *   <li>2 - Вивести книги в порядку найпопулярніші</li>
-     *   <li>3 - Повернутися назад</li>
-     * </ul>
-     * </p>
-     * @throws SQLException Якщо виникає помилка при доступі до бази даних.
-     * @throws IOException Якщо виникає помилка при роботі з файлами.
-     */
     public void display() throws SQLException, IOException {
         Scanner scanner = new Scanner(System.in);
         int choice;
@@ -81,3 +43,4 @@ public class BookMenu {
         } while (choice != 3);
     }
 }
+
