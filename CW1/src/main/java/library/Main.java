c08b7b3cf1de8ff138a06200c3cf1a9bab6ea56a
VSCRM <161849246+VSCRM@users.noreply.github.com>
2026-02-24 13:21:04 +0200

    Remove unnecessary whitespace in Main.java

--- CW1/src/main/java/library/Main.java
+++ CW1/src/main/java/library/Main.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 import java.io.IOException;
 
 public class Main {
-
     public static void main(String[] args) {
         try {
             MainMenu mainMenu = new MainMenu();
